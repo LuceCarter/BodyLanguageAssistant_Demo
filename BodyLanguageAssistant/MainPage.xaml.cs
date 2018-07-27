9123c69ebf1718b7f9fd19da3e087ab74720d56b
Luce Carter <luce@lucyscarter.co.uk>
2018-07-27 18:52:03 +0100

    first commit

---
+++ BodyLanguageAssistant/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BodyLanguageAssistant
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		public void AnalyseTextButtonClicked(object sender, EventArgs e)
+		{
+			Navigation.PushModalAsync(new AnalyseTextPage());
+		}
+
+		public void AnalyseBodyLanguageButtonClicked(object sender, EventArgs e)
+		{
+			Navigation.PushModalAsync(new NavigationPage(new AnalyseBodyLanguagePage()));
+		}
+	}
+}
---
+++ BodyLanguageAssistant/TextAnalyser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+using System.Collections.Generic;
+using Microsoft.Rest;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BodyLanguageAssistant
+{
+	public class TextAnalyser
+	{
+		public TextAnalyser()
+		{
+			//ITextAnalyticsClient client = new TextAnalyticsClient(new ApiKeyServiceClientCredentials());
+
+		}
+	}
+
+	class ApiKeyServiceClientCredentials : ServiceClientCredentials
+	{
+		public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			request.Headers.Add("Ocp-Apim-Subscription-Key", "b8222a8fcc96454ba813aaeae9d32c13");
+			return base.ProcessHttpRequestAsync(request, cancellationToken);
+		}
+	}
+}
